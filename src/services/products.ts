1d1d492210cc9261383af4af7c584c0b3b0fd34a
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-17 18:09:07 -0300

    feat: add product service functions for fetching all products, by category, and by ID

---
+++ src/services/products.ts
@@ -0,0 +1,28 @@
+import type { ProductsResponse, Product } from "@/types";
+
+export const getAllProducts = async (): Promise<ProductsResponse> => {
+  const response = await fetch('https://dummyjson.com/products');
+  if (!response.ok) {
+    throw new Error('Failed to fetch products');
+  }
+  
+  return response.json() ?? [];
+}
+
+export const getProductsByCategory = async (category: string): Promise<ProductsResponse> => {
+  const response = await fetch(`https://dummyjson.com/products/category/${category}`);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch products for category: ${category}`);
+  }
+  
+  return response.json() ?? [];
+}
+
+export const getProductById = async (id: string): Promise<Product> => {
+  const response = await fetch(`https://dummyjson.com/products/${id}`);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch product with id: ${id}`);
+  }
+  
+  return response.json();
+}
