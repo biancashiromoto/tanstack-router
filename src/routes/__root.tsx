be1169f694ccf38fecf706a3959d6176b06d7359
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-17 18:16:38 -0300

    feat: refactor root component to include AuthProvider and QueryClientProvider

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,22 +1,36 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { AuthProvider } from '@/context/AuthContext'
 import { TanstackDevtools } from '@tanstack/react-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { App } from './app'
+
+const queryClient = new QueryClient()
+
+function RootComponent() {
+  
+  return (
+    <>
+      <AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+        <TanstackDevtools
+          config={{
+            position: 'bottom-left',
+          }}
+          plugins={[
+            {
+              name: 'Tanstack Router',
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+          ]}
+        />
+      </AuthProvider>
+    </>
+  )
+}
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <Outlet />
-      <TanstackDevtools
-        config={{
-          position: 'bottom-left',
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
-    </>
-  ),
+  component: RootComponent,
 })
