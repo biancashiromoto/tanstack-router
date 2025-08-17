76b2b8ec6fb5fe65b891c0577936e1e55a756051
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-17 19:03:35 -0300

    feat: add loader to login route and update authentication check

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,11 +1,14 @@
 import { useAuth } from '@/context/AuthContext'
 import { getAllUsers } from '@/services/users'
 import { useQuery } from '@tanstack/react-query'
-import { createFileRoute, useNavigate } from '@tanstack/react-router'
+import { createFileRoute, useLoaderData, useNavigate } from '@tanstack/react-router'
 import { useState } from 'react'
 
 export const Route = createFileRoute('/login')({
-  component: Login
+  component: Login,
+  loader: async ({ context }) => {
+    return { user: context.user || null }
+  }
 })
 
 function Login() {
@@ -13,10 +16,11 @@ function Login() {
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
   const [isLoading, setIsLoading] = useState(false)
-  const { login, isAuthenticated } = useAuth()
+  const { login } = useAuth()
   const navigate = useNavigate()
+  const { user } = useLoaderData({ from: '/login' });
 
-  if (isAuthenticated) {
+  if (user) {
     navigate({ to: '/profile' })
     return null
   }
@@ -26,8 +30,6 @@ function Login() {
     queryFn: getAllUsers,
     enabled: false
   });
-
-  console.log('users', users?.map(user => ({ username: user.username, password: user.password })));
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
@@ -50,7 +52,7 @@ function Login() {
 
   return (
     <>
-      <h2>Login</h2>
+      <h2 className='subtitle'>Login</h2>
       <form onSubmit={handleSubmit}>
         {error && <div className="error-message">{error}</div>}
         <div>
