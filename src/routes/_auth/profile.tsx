ff1ab8b3950c1860ebf2e1d0d1e198464ca0a5cd
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-17 19:02:38 -0300

    feat: refactor profile route to use user ID from context and improve user data loading

--- src/routes/_auth/profile.tsx
+++ src/routes/_auth/profile.tsx
@@ -1,28 +1,36 @@
-import { useAuth } from '@/context/AuthContext'
-import { createFileRoute, redirect } from '@tanstack/react-router'
+import { getUserById } from '@/services/users'
+import { createFileRoute, useLoaderData } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_auth/profile')({
-  loader: () => {
-    const savedUser = localStorage.getItem('user')
-    if (!savedUser) {
-      throw redirect({
-        to: '/unauthenticated',
-      })
+  loader: async ({ context }) => {
+    const userId = context.user?.id
+    if (userId) {
+      try {
+        const user = await getUserById(Number(userId))
+        return { user }
+      } catch (error) {
+        console.error('Error loading user:', error)
+        return { user: null }
+      }
     }
+    return { user: null }
   },
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  const { user } = useAuth()
+  const { user } = useLoaderData({ from: '/_auth/profile' });
 
   return (
     <div className="profile-container">
-      <h2>Profile Page</h2>
+      <h2 className='subtitle'>Profile Page</h2>
       {user && (
         <div className="user-info">
-          <p><strong>Welcome, {user.firstName}!</strong></p>
+          <p className='text'><strong>Welcome, {user.firstName}!</strong></p>
           <img src={user.image} alt="User Avatar" className="avatar" />
+          <p className='text'><strong>Address:</strong> {user.address.address} - {user.address.city}, {user.address.state} - {user.address.country}</p>
+          <p className='text'><strong>Email:</strong> {user.email}</p>
+          <p className='text'><strong>Phone:</strong> {user.phone}</p>
         </div>
       )}
     </div>
