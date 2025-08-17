11a314c1714233b4b318d040ed9b2bd1ace4c35b
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-17 18:16:30 -0300

    feat: add category-based product listing route with loader and display component

---
+++ src/routes/products/$category/index.tsx
@@ -0,0 +1,38 @@
+import { getProductsByCategory } from '@/services/products';
+import type { Product } from '@/types';
+import { createFileRoute, useLoaderData, useNavigate, useParams } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/products/$category/')({
+  component: RouteComponent,
+  loader: async ({ params }) => {
+    const { category } = params;
+    if (!category) {
+      throw new Error('Category is required');
+    }
+    const products = await getProductsByCategory(category);
+    return { products };
+  },
+})
+
+function RouteComponent() {
+  const { category } = useParams({ from: '/products/$category/' });
+  const { products } = useLoaderData({ from: '/products/$category/' });
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      {!products && <p>Loading...</p>}
+      <h2>Products in {category}</h2>
+      {products && (
+        <ul className='product-list'>
+          {products.products.map((product: Product) => (
+            <li key={product.id} className="product-item" onClick={ () => navigate({ to: `/products/${category}/${product.id}` }) }>
+              <img src={product.images[0]} alt={product.title} className="product-thumbnail" />
+              <strong>{product.title}</strong> - ${product.price}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
