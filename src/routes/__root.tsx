9c7b2d82a39bbeb7e3351f5d2d5b812942715c2f
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-17 19:03:48 -0300

    feat: refactor root route to use context for user data

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,14 @@
 import { AuthProvider } from '@/context/AuthContext'
 import { TanstackDevtools } from '@tanstack/react-devtools'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { createRootRoute } from '@tanstack/react-router'
+import { createRootRouteWithContext } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { App } from './app'
+import type { User } from '@/types'
+
+interface RouterContext {
+  user: User | null
+}
 
 const queryClient = new QueryClient()
 
@@ -31,6 +36,6 @@ function RootComponent() {
   )
 }
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<RouterContext>()({
   component: RootComponent,
 })
