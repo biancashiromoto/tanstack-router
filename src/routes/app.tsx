569d1f6a3fa1ed97cad745de2da1cdfcbd7a1b94
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-20 19:04:52 -0300

    feat: add HeadContent component to App for improved SEO and structure

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -2,15 +2,19 @@ import { useAuth } from '@/context/AuthContext';
 import { Outlet } from '@tanstack/react-router';
 import Avatar from './-components/Avatar';
 import Navbar from './-components/Navbar';
+import { HeadContent } from '@tanstack/react-router';
 
 export function App() {
   const { user } = useAuth();
 
   return (
-    <div>
-      {user && <Avatar user={user} />}
-      <Navbar />
-      <Outlet />
-    </div>
+    <>
+      <HeadContent />
+      <div>
+        {user && <Avatar user={user} />}
+        <Navbar />
+        <Outlet />
+      </div>
+    </>
   )
 }
