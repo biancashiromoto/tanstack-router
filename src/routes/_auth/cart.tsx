e0d41d8f67a3f49e9357b0aec4ed4692098a103f
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-20 19:03:10 -0300

    feat: simplify cart loading logic and enhance cart item rendering

--- src/routes/_auth/cart.tsx
+++ src/routes/_auth/cart.tsx
@@ -2,50 +2,34 @@ import { useAuth } from '@/context/AuthContext'
 import { getUsersCartById } from '@/services/users'
 import type { Product } from '@/types'
 import { createFileRoute, useLoaderData } from '@tanstack/react-router'
+import CartItem from '../-components/CartItem'
 
 export const Route = createFileRoute('/_auth/cart')({
   component: RouteComponent,
   loader: async ({ context }) => {
-    const userId = context.user?.id
-    if (userId) {
-      try {
-        const cart = await getUsersCartById(Number(userId))
-        console.log('Cart loaded from context:', cart)
-        return { cart }
-      } catch (error) {
-        console.error('Error loading cart:', error)
-        return { cart: null }
-      }
-    }
-    return { cart: null }
+    const userId = context.user?.id;
+    const cart = await getUsersCartById(Number(userId))
+    return { cart: cart[0] ?? { products: [] } }
   }
 })
 
 function RouteComponent() {
-  const { user } = useAuth()
-  const { cart } = useLoaderData({ from: '/_auth/cart' });
+  const { user } = useAuth();
+  const { cart: { products, total } } = useLoaderData({ from: '/_auth/cart' });
 
   return (
-    <div className="cart-container">
+    <>
       <h2 className='subtitle'>{user?.firstName}'s Cart</h2>
-      {cart ? (
-        <div>
-          {
-            cart.length > 0 ? cart.map((item: Product) => (
-              <div key={item.id} className="cart-item">
-                <p className="text"><strong>Product Title:</strong> {item.title}</p>
-                <p className="text"><strong>Price:</strong> ${item.price}</p>
-                <img src={item.images[0]} alt={item.title} className="cart-item-image" />
-                <p className="text"><strong>Description:</strong> {item.description}</p>
-                <p className="text"><strong>Category:</strong> {item.category}</p>
-                <p className="text"><strong>Brand:</strong> {item.brand}</p>
-              </div>
-            )) : <p>No items in cart</p>
-          }
+      {products ? (
+        <div className='cart-list'>
+          {products.length > 0 ? products.map((item: Product) => (
+            <CartItem key={item.id} item={item} />
+          )) : <p>No items in cart</p>}
         </div>
       ) : (
         <p>No cart data available</p>
       )}
-    </div>
+      {products.length > 0 && <h3 className='subtitle cart-price'>Total: ${total}</h3>}
+    </>
   )
 }
