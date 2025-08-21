f6fcc14d07ef12df36c77196d2fd843072c8cf2a
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-20 21:06:25 -0300

    feat: refactor App component to integrate category fetching and loading/error states

--- src/routes/-components/app.tsx
+++ src/routes/-components/app.tsx
@@ -3,18 +3,24 @@ import { Outlet } from '@tanstack/react-router';
 import Avatar from './Avatar';
 import Navbar from './Navbar';
 import { HeadContent } from '@tanstack/react-router';
+import useFetchCategories from '@/hooks/useFetchCategories';
+import CategoriesMenu from './CategoriesMenu';
 
 export function App() {
   const { user } = useAuth();
+  const { data: categories, isLoading, error } = useFetchCategories();
 
   return (
     <>
       <HeadContent />
-      <div>
+      <main className={`main ${user ? 'auth' : ''}`}>
         {user && <Avatar user={user} />}
         <Navbar />
+        {isLoading && <div>Loading categories...</div>}
+        {error && <div>Error loading categories</div>}
+        {categories && <CategoriesMenu categories={categories} />}
         <Outlet />
-      </div>
+      </main>
     </>
-  )
+  );
 }
