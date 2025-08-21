0bc62a724d1369c9d72601ee1dc72e3614c0bdd1
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-20 21:07:16 -0300

    feat: add CategoriesMenu component for displaying product categories

---
+++ src/routes/-components/CategoriesMenu/index.tsx
@@ -0,0 +1,24 @@
+import { Link } from "@tanstack/react-router";
+
+type CategoriesMenuProps = {
+  categories: string[];
+};
+
+const CategoriesMenu = ({
+  categories,
+}: CategoriesMenuProps) => {
+  return (
+    <div className='categories-container'>
+      <h3>Categories</h3>
+      <ul className='categories-list'>
+        {categories.map((category: string) => (
+          <Link key={category} className='category-item' to={`/products/${category}`} activeProps={{ className: 'active' }} preload={false}>
+            {category}
+          </Link>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default CategoriesMenu
