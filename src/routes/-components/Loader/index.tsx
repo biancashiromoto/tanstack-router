cd51235a43f35feb0b3acd0203c3deeb66795448
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-20 21:06:09 -0300

    feat: add Loader component with spinner styles

---
+++ src/routes/-components/Loader/index.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import './Loader.scss';
+
+const Loader: React.FC = () => (
+  <div className="loader-container">
+    <div className="loader-spinner"></div>
+  </div>
+);
+
+export default Loader;
