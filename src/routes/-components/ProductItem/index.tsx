ce4c194b137602d9843905d75d55110aeffdd53f
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-23 19:30:48 -0300

    fix: replace navigate with Link component in ProductItem for improved routing

--- src/routes/-components/ProductItem/index.tsx
+++ src/routes/-components/ProductItem/index.tsx
@@ -1,5 +1,5 @@
 import type { Product } from "@/types"
-import { useNavigate } from "@tanstack/react-router";
+import { Link } from "@tanstack/react-router";
 
 type ProductItemProps = {
   product: Product;
@@ -7,12 +7,12 @@ type ProductItemProps = {
 }
 
 const ProductItem = ({ product, category }: ProductItemProps) => {
-  const navigate = useNavigate();
-  
   return (
-    <li key={product.id} className="product-item" onClick={ () => navigate({ to: `/products/${category}/${product.id}` }) }>
-      <img src={product.images[0]} alt={product.title} className="product-thumbnail" />
-      <strong>{product.title}</strong> - ${product.price}
+    <li key={product.id} className="product-item">
+      <Link to={`/${category}/${product.id}`}>
+        <img src={product.images[0]} alt={product.title} className="product-thumbnail" />
+        <strong>{product.title}</strong> - ${product.price}
+      </Link>
     </li>
   )
 }
