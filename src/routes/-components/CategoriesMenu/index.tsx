bc962d77e16515fbdfcb8817ac7a4ff86534f707
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-23 19:22:12 -0300

    fix: update link path in CategoriesMenu component

--- src/routes/-components/CategoriesMenu/index.tsx
+++ src/routes/-components/CategoriesMenu/index.tsx
@@ -12,7 +12,7 @@ const CategoriesMenu = ({
       <h3>Categories</h3>
       <ul className='categories-list'>
         {categories.map((category: string) => (
-          <Link key={category} className='category-item' to={`/products/${category}`} activeProps={{ className: 'active' }} preload={false}>
+          <Link key={category} className='category-item' to={`/${category}`} activeProps={{ className: 'active' }} preload={false}>
             {category}
           </Link>
         ))}
