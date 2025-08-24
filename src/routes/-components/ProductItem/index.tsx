3d74648bf19001091fbb44a295982f5af8757e0c
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-24 16:32:13 -0300

    fix: ensure preload is set to false for product links

--- src/routes/-components/ProductItem/index.tsx
+++ src/routes/-components/ProductItem/index.tsx
@@ -14,6 +14,7 @@ const ProductItem = ({ product }: ProductItemProps) => {
           category: product.category, 
           id: product.id.toString() 
         }}
+        preload={false}
       >
         <img src={product.images[0]} alt={product.title} className="product-thumbnail" />
         <strong>{product.title}</strong> - ${product.price}
