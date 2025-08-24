c8df48fd2a2c4eaeca2932cb65392ebe77491977
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-24 19:36:47 -0300

    refactor: update Loader component for improved styling and flexibility

--- src/routes/-components/Loader/index.tsx
+++ src/routes/-components/Loader/index.tsx
@@ -1,10 +1,12 @@
+import React from "react";
+import "./Loader.scss";
 
-import React from 'react';
-import './Loader.scss';
-
-const Loader: React.FC = () => (
+const Loader: React.FC<{ color?: string }> = ({ color = "#3498db" }) => (
   <div className="loader-container">
-    <div className="loader-spinner"></div>
+    <div
+      className="loader-spinner"
+      style={{ borderTop: `4px solid ${color}` }}
+    />
   </div>
 );
 
