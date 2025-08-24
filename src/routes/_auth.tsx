6e28c097b327ad371274e7d4592879669fa58d84
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-24 19:36:21 -0300

    refactor: format code for consistency and improve readability in _auth route

--- src/routes/_auth.tsx
+++ src/routes/_auth.tsx
@@ -1,18 +1,23 @@
-import { createFileRoute, redirect, Outlet, useRouterState } from '@tanstack/react-router';
-import Loader from '@/routes/-components/Loader';
+import {
+  createFileRoute,
+  redirect,
+  Outlet,
+  useRouterState,
+} from "@tanstack/react-router";
+import Loader from "@/routes/-components/Loader";
 
 export const Route = createFileRoute("/_auth")({
   component: AuthLayout,
   beforeLoad: async ({ context }) => {
     if (!context.user) {
-      throw redirect({ to: '/unauthenticated' })
+      throw redirect({ to: "/unauthenticated" });
     }
-    return {}
+    return {};
   },
-})
+});
 
 function AuthLayout() {
-  const isLoading = useRouterState({ select: s => s.status === 'pending' });
-  
+  const isLoading = useRouterState({ select: (s) => s.status === "pending" });
+
   return isLoading ? <Loader /> : <Outlet />;
 }
