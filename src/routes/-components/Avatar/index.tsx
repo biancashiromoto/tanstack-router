732d9f6102690a52a59b48946e1c2a9d0864d165
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-24 20:59:15 -0300

    refactor: enhance Avatar component with navigation and improve structure

--- src/routes/-components/Avatar/index.tsx
+++ src/routes/-components/Avatar/index.tsx
@@ -1,13 +1,23 @@
-import type { User } from "@/types"
-import { RxAvatar } from "react-icons/rx"
+import type { User } from "@/types";
+import { useNavigate } from "@tanstack/react-router";
+import { RxAvatar } from "react-icons/rx";
 
 const Avatar = ({ user }: { user: User }) => {
+  const navigate = useNavigate();
   return (
-    <div className="avatar-container">
-      {user.image ? <img className="avatar-img" src={user.image} alt="User Avatar" /> : <RxAvatar />}
-      <p className="avatar-name">{user.email}</p>
+    <div
+      className="avatar-container"
+      onClick={() => navigate({ to: `/profile` })}
+      role="link"
+    >
+      {user.image ? (
+        <img className="avatar-img" src={user.image} alt="User Avatar" />
+      ) : (
+        <RxAvatar />
+      )}
+      <p className="avatar-email">{user.email}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Avatar
+export default Avatar;
