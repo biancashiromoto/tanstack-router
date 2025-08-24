5cbcd6b5dbf62ee2eb392ba22feaee71b8bebc35
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-24 16:12:06 -0300

    fix: refactor product route loader to use queryClient for data fetching

--- src/routes/_product.$category.$id.tsx
+++ src/routes/_product.$category.$id.tsx
@@ -1,22 +1,43 @@
 import { getProductById } from '@/services/products';
+import type { Product } from '@/types';
+import { queryOptions } from '@tanstack/react-query';
 import { createFileRoute, useLoaderData } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_product/$category/$id')({
   component: RouteComponent,
-  loader: async ({ params }) => {
+  loader: async ({ params, context }) => {
+    const queryClient = context?.queryClient;
     const { id } = params;
-    if (!id) {
-      throw new Error('Product ID is required');
-    }
-    const product = await getProductById(Number(id));
-    if (!product) throw new Error('Product not found');
-    return { product };
+    if (!id) throw new Error('Product ID is required');
+    return queryClient.ensureQueryData(
+      queryOptions({
+        queryKey: ['product', id],
+        queryFn: async () => {
+          const product = await getProductById(Number(id));
+          if (!product) throw new Error('Product not found');
+          return product;
+        },
+        staleTime: 1000 * 60 * 5, // 5 minutes
+      })
+    );
   },
-  errorComponent: () => <p>Error loading product details.</p>,
+  errorComponent: ({ error }) => {
+    return <p>Error loading product details: {error.message}</p>;
+  },
+  head: ({ loaderData }: { loaderData?: Product }) => {
+    if (!loaderData) return { meta: [{ title: 'Product not found' }] };
+    return {
+      meta: [
+        {
+          title: loaderData.title,
+        }
+      ]
+    };
+  },
 });
 
 function RouteComponent() {
-  const { product } = useLoaderData({ from: '/_product/$category/$id' });
+  const product = useLoaderData({ from: '/_product/$category/$id' });
 
   const productRating = product.rating ? new Array(Math.ceil(product.rating)).fill('⭐') : null;
 
