de4c1ce173d73fb6b002be88b8a5aede7afc820a
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-24 21:10:22 -0300

    refactor: improve code formatting and structure in cart route

--- src/routes/_auth.cart.tsx
+++ src/routes/_auth.cart.tsx
@@ -1,42 +1,50 @@
-import { useAuth } from '@/context/AuthContext'
-import { getUsersCartById } from '@/services/users'
-import type { Product } from '@/types'
-import { createFileRoute, useLoaderData } from '@tanstack/react-router'
-import CartItem from '@/routes/-components/CartItem';
+import { useAuth } from "@/context/AuthContext";
+import { getUsersCartById } from "@/services/users";
+import type { Product } from "@/types";
+import { createFileRoute, useLoaderData } from "@tanstack/react-router";
+import CartItem from "@/routes/-components/CartItem";
 
-export const Route = createFileRoute('/_auth/cart')({
+export const Route = createFileRoute("/_auth/cart")({
   component: RouteComponent,
   loader: async ({ context }) => {
     const userId = context.user?.id;
-    const cart = await getUsersCartById(Number(userId))
-    return { cart: cart[0] ?? { products: [] } }
+    const cart = await getUsersCartById(Number(userId));
+    return { cart: cart[0] ?? { products: [] } };
   },
   head: () => ({
     meta: [
       {
-        title: 'Cart',
-      }
-    ]
+        title: "Cart",
+      },
+    ],
   }),
-})
+});
 
 function RouteComponent() {
   const { user } = useAuth();
-  const { cart: { products, total } } = useLoaderData({ from: '/_auth/cart' });
+  const {
+    cart: { products, total },
+  } = useLoaderData({ from: "/_auth/cart" });
 
   return (
-    <>
-      <h2 className='subtitle'>{user?.firstName}'s Cart</h2>
+    <div className="cart-container">
+      <h2 className="subtitle">{user?.firstName}'s Cart</h2>
       {products ? (
-        <div className='cart-list'>
-          {products.length > 0 ? products.map((item: Product) => (
-            <CartItem key={item.id} item={item} />
-          )) : <p>No items in cart</p>}
+        <div className="cart-list">
+          {products.length > 0 ? (
+            products.map((item: Product) => (
+              <CartItem key={item.id} item={item} />
+            ))
+          ) : (
+            <p>No items in cart</p>
+          )}
         </div>
       ) : (
         <p>No cart data available</p>
       )}
-      {products.length > 0 && <h3 className='subtitle cart-price'>Total: ${total}</h3>}
-    </>
-  )
+      {products.length > 0 && (
+        <h3 className="subtitle cart-price">Total: ${total}</h3>
+      )}
+    </div>
+  );
 }
