c3248857332cf0ec9737bdb38478b15af87b7265
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-24 21:18:58 -0300

    refactor: clean up app component by removing unused imports and loading/error states

--- src/routes/-components/app.tsx
+++ src/routes/-components/app.tsx
@@ -1,26 +1,19 @@
 import { useAuth } from "@/context/AuthContext";
-import { Outlet, useParams } from "@tanstack/react-router";
-// import Navbar from './Navbar';
+import { Outlet } from "@tanstack/react-router";
 import useFetchCategories from "@/hooks/useFetchCategories";
 import { HeadContent } from "@tanstack/react-router";
 import CategoriesMenu from "./CategoriesMenu";
 import Header from "./Header";
-import BreadcrumbProducts from "./BreadcrumbProducts";
 
 export function App() {
   const { user } = useAuth();
-  const { data: categories, isLoading, error } = useFetchCategories();
-  const { category, id } = useParams({ from: "" });
+  const { data: categories } = useFetchCategories();
 
   return (
     <>
       <HeadContent />
       <main className={`main ${user ? "auth" : ""}`}>
         <Header />
-        {/* <Navbar /> */}
-        {category && <BreadcrumbProducts category={category} productId={id} />}
-        {isLoading && <div>Loading categories...</div>}
-        {error && <div>Error loading categories</div>}
         {categories && <CategoriesMenu categories={categories} />}
         <Outlet />
       </main>
