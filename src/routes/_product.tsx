1422c7b2ba11fdf36dc1f44928567c78d0422b5e
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-08-24 21:02:13 -0300

    refactor: implement loader for product route to fetch products by category

--- src/routes/_product.tsx
+++ src/routes/_product.tsx
@@ -4,9 +4,28 @@ import {
   useRouterState,
 } from "@tanstack/react-router";
 import Loader from "./-components/Loader";
+import { getProductsByCategory } from "@/services/products";
+import type { Product } from "@/types";
+
+export type LoaderData = {
+  products: {
+    products: Product[];
+  };
+};
+
+export type LoaderParams = {
+  params: {
+    category: Product["category"];
+  };
+};
 
 export const Route = createFileRoute("/_product")({
   component: RouteComponent,
+  loader: async ({ params }: LoaderParams): Promise<LoaderData> => {
+    const category = params.category;
+    const products = await getProductsByCategory(category);
+    return { products };
+  },
 });
 
 function RouteComponent() {
