51c3ade2cb0fb1878eb70d933ef042326c781f60
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-07 19:03:53 -0300

    refactor: wrap App component in MUI Box for improved layout and styling

--- src/routes/-components/app.tsx
+++ src/routes/-components/app.tsx
@@ -1,10 +1,13 @@
+import { Box } from "@mui/material";
 import { HeadContent, Outlet } from "@tanstack/react-router";
+import Header from "./Header";
 
 export function App() {
   return (
-    <>
+    <Box sx={{ px: 2, py: 1 }}>
       <HeadContent />
+      <Header />
       <Outlet />
-    </>
+    </Box>
   );
 }
