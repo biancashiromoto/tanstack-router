c67881a260ac9e6c9abe22acdb849e5ca0acc76f
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-07 19:04:03 -0300

    refactor: simplify App component by removing unused state and search functionality, and use MUI Box for layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,7 @@
 import { useAuth } from "@/context/AuthContext";
 import LoginForm from "@/routes/-components/LoginForm";
-import { searchProducts } from "@/services/products";
-import type { Product } from "@/types";
+import { Box } from "@mui/material";
 import { createFileRoute } from "@tanstack/react-router";
-import { useState } from "react";
-import Header from "./-components/Header";
 
 export const Route = createFileRoute("/")({
   component: App,
@@ -12,31 +9,6 @@ export const Route = createFileRoute("/")({
 
 function App() {
   const { user } = useAuth();
-  const [search, setSearch] = useState<string>("");
-  const [options, setOptions] = useState<Product[]>([]);
 
-  const handleSearch = async (query: string) => {
-    try {
-      const results = await searchProducts(query);
-      setOptions(results.products);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleSearchChange = (value: string) => {
-    setSearch(value);
-    handleSearch(value);
-  };
-
-  return (
-    <div className="home-container">
-      <Header
-        search={search}
-        options={options}
-        handleSearchChange={handleSearchChange}
-      />
-      {!user && <LoginForm />}
-    </div>
-  );
+  return <Box className="home-container">{!user && <LoginForm />}</Box>;
 }
