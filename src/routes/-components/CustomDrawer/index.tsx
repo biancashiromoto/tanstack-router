02608a2bc67feea6dc38fd99cc9c2649135f296d
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-07 17:58:17 -0300

    feat: add CustomDrawer component for category navigation

---
+++ src/routes/-components/CustomDrawer/index.tsx
@@ -0,0 +1,57 @@
+import { IconButton, Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import { Link, useRouteContext } from "@tanstack/react-router";
+import { useState } from "react";
+import { FiMenu } from "react-icons/fi";
+
+export default function CustomDrawer() {
+  const [open, setOpen] = useState(false);
+  const { categories } = useRouteContext({ from: "__root__" });
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        <Typography variant="h6">Categories</Typography>
+        {categories.map((category: string) => (
+          <ListItem key={category} className="category-item" sx={{ p: 0.25 }}>
+            <Link
+              to={`/${category}`}
+              params={{ category }}
+              style={{ width: "100%" }}
+            >
+              <ListItemText primary={category} />
+            </Link>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </Box>
+  );
+
+  return (
+    <Box sx={{ textAlign: "right" }}>
+      <IconButton
+        onClick={toggleDrawer(true)}
+        sx={{
+          color: "black",
+          ml: 5,
+          fontSize: 36,
+        }}
+      >
+        <FiMenu />
+      </IconButton>
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+    </Box>
+  );
+}
