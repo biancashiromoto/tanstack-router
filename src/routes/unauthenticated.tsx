f2486330a0fbe1e34c85852c11f9017269cfe2ae
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-07 20:11:12 -0300

    fix: replace div and HTML elements with MUI Box and Typography for consistency

--- src/routes/unauthenticated.tsx
+++ src/routes/unauthenticated.tsx
@@ -1,14 +1,17 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { Box, Typography } from "@mui/material";
+import { createFileRoute, Link } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/unauthenticated')({
+export const Route = createFileRoute("/unauthenticated")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
   return (
-    <div>
-      <h2 className='subtitle'>Unauthenticated</h2>
-      <p className='text'>Please <Link to="/">sign in</Link> to access this page.</p>
-    </div>
-  )
+    <Box>
+      <Typography className="subtitle">Unauthenticated</Typography>
+      <Typography className="text">
+        Please <Link to="/">sign in</Link> to access this page.
+      </Typography>
+    </Box>
+  );
 }
