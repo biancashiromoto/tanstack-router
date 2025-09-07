f917404b2ea6c210340a5cc7241d95589e411849
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-07 17:59:47 -0300

    feat: implement search functionality in App component with dynamic product options

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,42 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-import "../App.scss";
+import { useAuth } from "@/context/AuthContext";
 import LoginForm from "@/routes/-components/LoginForm";
+import { searchProducts } from "@/services/products";
+import type { Product } from "@/types";
+import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
+import Header from "./-components/Header";
 
 export const Route = createFileRoute("/")({
   component: App,
-  beforeLoad: async ({ context }) => {
-    const user = context.user;
-    if (user) throw redirect({ to: "/profile" });
-    return { user: null };
-  },
 });
 
 function App() {
+  const { user } = useAuth();
+  const [search, setSearch] = useState<string>("");
+  const [options, setOptions] = useState<Product[]>([]);
+
+  const handleSearch = async (query: string) => {
+    try {
+      const results = await searchProducts(query);
+      setOptions(results.products);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSearchChange = (value: string) => {
+    setSearch(value);
+    handleSearch(value);
+  };
+
   return (
     <div className="home-container">
-      <h1>TanStack Router Example</h1>
-      <LoginForm />
+      <Header
+        search={search}
+        options={options}
+        handleSearchChange={handleSearchChange}
+      />
+      {!user && <LoginForm />}
     </div>
   );
 }
