c461c0c6b83fa4ef0da906f3734c1c5a8bf38b1d
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-07 17:59:34 -0300

    refactor: simplify App component by removing unused authentication and category fetching logic

--- src/routes/-components/app.tsx
+++ src/routes/-components/app.tsx
@@ -1,22 +1,10 @@
-import { useAuth } from "@/context/AuthContext";
-import { Outlet } from "@tanstack/react-router";
-import useFetchCategories from "@/hooks/useFetchCategories";
-import { HeadContent } from "@tanstack/react-router";
-import CategoriesMenu from "./CategoriesMenu";
-import Header from "./Header";
+import { HeadContent, Outlet } from "@tanstack/react-router";
 
 export function App() {
-  const { user } = useAuth();
-  const { data: categories } = useFetchCategories();
-
   return (
     <>
       <HeadContent />
-      <main className={`main ${user ? "auth" : ""}`}>
-        <Header />
-        {categories && <CategoriesMenu categories={categories} />}
-        <Outlet />
-      </main>
+      <Outlet />
     </>
   );
 }
