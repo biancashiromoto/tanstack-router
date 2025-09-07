0f15d817b3fc5effaf774f8562d52cc4216b5eb9
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-07 19:20:13 -0300

    refactor: simplify SearchBar component by removing unused state and handlers

--- src/routes/-components/SearchBar/index.tsx
+++ src/routes/-components/SearchBar/index.tsx
@@ -1,34 +1,7 @@
-import type { Product } from "@/types";
-import { Box } from "@mui/material";
 import CustomAutocomplete from "../Autocomplete";
-import { searchProducts } from "@/services/products";
-import { useState } from "react";
 
 const SearchBar = () => {
-  const [search, setSearch] = useState<string>("");
-  const [options, setOptions] = useState<Product[]>([]);
-
-  const handleSearch = async (query: string) => {
-    try {
-      const results = await searchProducts(query);
-      setOptions(results.products);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleSearchChange = (value: string) => {
-    setSearch(value);
-    handleSearch(value);
-  };
-
-  return (
-    <CustomAutocomplete
-      handleChange={handleSearchChange}
-      search={search}
-      options={options}
-    />
-  );
+  return <CustomAutocomplete />;
 };
 
 export default SearchBar;
