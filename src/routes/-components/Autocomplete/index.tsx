b955c63a7564736668f659f5e1a0b2c36fe63257
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-07 19:20:37 -0300

    refactor: simplify CustomAutocomplete by removing props and using local state management

--- src/routes/-components/Autocomplete/index.tsx
+++ src/routes/-components/Autocomplete/index.tsx
@@ -1,33 +1,32 @@
+import useSearchProducts from "@/hooks/useSearchProducts";
 import type { Product } from "@/types";
 import { FormControl, Typography } from "@mui/material";
 import Autocomplete from "@mui/material/Autocomplete";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import { useLocation } from "@tanstack/react-router";
 import { useNavigate } from "@tanstack/react-router";
+import { useEffect } from "react";
 
-export type AutocompleteProps = {
-  handleChange: (value: string) => void;
-  search: string;
-  options: Product[];
-};
-
-export default function CustomAutocomplete({
-  handleChange,
-  search,
-  options,
-}: AutocompleteProps) {
+export default function CustomAutocomplete() {
   const navigate = useNavigate();
+  const location = useLocation();
+  const { search, options, handleSearchChange } = useSearchProducts();
 
   const filteredProducts = options.filter((option) =>
     option.title.toLowerCase().includes(search.toLowerCase())
   );
+
+  useEffect(() => {
+    if (location.pathname === "/") handleSearchChange("");
+  }, [location]);
 
   return (
     <FormControl fullWidth>
       <Autocomplete
         size="medium"
         inputValue={search}
-        onInputChange={(_, value) => handleChange(value)}
+        onInputChange={(_, value) => handleSearchChange(value)}
         clearOnEscape
         onChange={(_, value) => {
           if (!value) {
