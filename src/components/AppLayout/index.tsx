4dd8cfd53ffa80dd65d7f2abf7025f76eac1d98d
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-08 20:28:06 -0300

    fix: add BreadcrumbProducts component to AppLayout

--- src/components/AppLayout/index.tsx
+++ src/components/AppLayout/index.tsx
@@ -1,10 +1,12 @@
 import { Box } from "@mui/material";
-import Header from "../Header";
+import BreadcrumbProducts from "@/components/BreadcrumbProducts";
+import Header from "@/components/Header";
 
 const AppLayout = ({ children }: { children?: React.ReactNode }) => {
   return (
     <Box sx={{ maxWidth: 1000, mx: "auto", p: 2 }}>
       <Header />
+      <BreadcrumbProducts />
       {children}
     </Box>
   );
