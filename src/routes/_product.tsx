5eac994cbb4d268cffce188cec40d7299ff9d2fc
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-08 20:28:25 -0300

    fix: simplify Route component by removing unnecessary imports and restructuring loader

--- src/routes/_product.tsx
+++ src/routes/_product.tsx
@@ -1,34 +1,12 @@
 import { getProductsByCategory } from "@/services/products";
-import { Box } from "@mui/material";
-import {
-  createFileRoute,
-  Outlet,
-  useParams,
-  useRouterState,
-} from "@tanstack/react-router";
-import BreadcrumbProducts from "@/components/BreadcrumbProducts";
-import Loader from "@/components/Loader";
 import type { LoaderData, LoaderParams } from "@/types/_product.types";
+import { createFileRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_product")({
-  component: RouteComponent,
+  component: () => <Outlet />,
   loader: async ({ params }: LoaderParams): Promise<LoaderData> => {
     const category = params.category;
-    const products = await getProductsByCategory(category);
+    const { products } = await getProductsByCategory(category);
     return { category, products };
   },
 });
-
-function RouteComponent() {
-  const isLoading = useRouterState({ select: (s) => s.status === "pending" });
-  const { category, id } = useParams({ from: "" });
-
-  if (isLoading) return <Loader />;
-
-  return (
-    <Box sx={{ mx: "auto", p: 1 }}>
-      <BreadcrumbProducts category={category} productId={id} />
-      <Outlet />
-    </Box>
-  );
-}
