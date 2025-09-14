59b215f596a6a48b121e2ef585043ca8e5f121bf
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-14 16:25:02 -0300

    refactor: replace navigate with Link for cart item navigation

--- src/components/CartItem/index.tsx
+++ src/components/CartItem/index.tsx
@@ -1,53 +1,47 @@
 import type { Product } from "@/types";
 import { Box, Typography } from "@mui/material";
-import { useNavigate } from "@tanstack/react-router";
+import { Link } from "@tanstack/react-router";
 
 type CartItemProps = {
   item: Product;
 };
 
 const CartItem = ({ item }: CartItemProps) => {
-  const navigate = useNavigate();
-
-  const handleClick = () =>
-    navigate({
-      to: "/$category/$id",
-      params: { category: item.category, id: item.id.toString() },
-      mask: {
-        to: "/cart/$id",
-        params: { id: item.id.toString() },
-      },
-    });
-
   return (
-    <Box
-      onClick={handleClick}
-      sx={{
-        cursor: "pointer",
-        display: "flex",
-        alignItems: "center",
-        gap: 2,
-        padding: 1,
-        border: "1px solid #ccc",
-        borderRadius: 1,
-        justifyContent: "space-between",
-      }}
+    <Link
+      to="/cart/$productId"
+      params={{ productId: String(item.id), category: item.category }}
+      mask={{ to: `/${item.category}/${item.id}` }}
+      style={{ textDecoration: "none" }}
     >
       <Box
-        component="img"
-        src={item.thumbnail}
-        alt={item.title}
-        className="cart-thumbnail"
-        width="75px"
-        height="75px"
-        sx={{ objectFit: "cover", borderRadius: 1 }}
-      />
-      <Typography variant="body2" sx={{ flexGrow: 1, maxWidth: "150px" }}>
-        {item.title}
-      </Typography>
-      <Typography>${item.price}</Typography>
-      <Typography sx={{ padding: "0 16px" }}>{item.quantity}</Typography>
-    </Box>
+        sx={{
+          cursor: "pointer",
+          display: "flex",
+          alignItems: "center",
+          gap: 2,
+          padding: 1,
+          border: "1px solid #ccc",
+          borderRadius: 1,
+          justifyContent: "space-between",
+        }}
+      >
+        <Box
+          component="img"
+          src={item.thumbnail}
+          alt={item.title}
+          className="cart-thumbnail"
+          width="75px"
+          height="75px"
+          sx={{ objectFit: "cover", borderRadius: 1 }}
+        />
+        <Typography variant="body2" sx={{ flexGrow: 1, maxWidth: "150px" }}>
+          {item.title}
+        </Typography>
+        <Typography>${item.price}</Typography>
+        <Typography sx={{ padding: "0 16px" }}>{item.quantity}</Typography>
+      </Box>
+    </Link>
   );
 };
 
