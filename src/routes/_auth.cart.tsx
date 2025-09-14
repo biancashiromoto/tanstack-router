d75ef8a152d18c14aeef17435c5403f41eec8ca1
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-14 16:52:30 -0300

    refactor: simplify cart component by removing loader and integrating useCart hook

--- src/routes/_auth.cart.tsx
+++ src/routes/_auth.cart.tsx
@@ -1,22 +1,13 @@
-import { useAuth } from "@/context/AuthContext";
-import { getUsersCartById } from "@/services/users";
-import type { Product } from "@/types";
-import { createFileRoute, useLoaderData } from "@tanstack/react-router";
 import CartItem from "@/components/CartItem";
+import Loader from "@/components/Loader";
+import { useAuth } from "@/context/AuthContext";
+import useCart from "@/hooks/useCart";
+import type { Product } from "@/types";
 import { Box, Typography } from "@mui/material";
-import { queryOptions } from "@tanstack/react-query";
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_auth/cart")({
   component: RouteComponent,
-  loader: async ({ context }) => {
-    return await context?.queryClient.ensureQueryData(
-      queryOptions({
-        queryKey: ["cart", context.user?.id],
-        queryFn: () => getUsersCartById(context.user?.id),
-        staleTime: 1000 * 60 * 5, // 5 minutes
-      })
-    );
-  },
   head: () => ({
     meta: [
       {
@@ -28,14 +19,18 @@ export const Route = createFileRoute("/_auth/cart")({
 
 function RouteComponent() {
   const { user } = useAuth();
-  const cart = useLoaderData({ from: "/_auth/cart" });
+  const { cart, isLoading } = useCart();
+
+  if (isLoading) return <Loader />;
+
+  if (!cart) return <Typography>No cart data available</Typography>;
 
   return (
     <Box className="cart-container">
       <Typography variant="h6" sx={{ marginBottom: 2 }}>
         {user?.firstName}'s Cart
       </Typography>
-      {cart.products.length ? (
+      {cart?.products.length ? (
         <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
           {cart.products.length > 0 ? (
             cart.products.map((item: Product) => (
@@ -57,7 +52,7 @@ function RouteComponent() {
             textAlign: "right",
           }}
         >
-          Total: ${cart.total}
+          Total: ${cart.totalProducts.toFixed(2)}
         </Typography>
       )}
     </Box>
