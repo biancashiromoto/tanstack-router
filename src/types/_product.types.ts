a765092def7be909bb5764565ec12c73039d2d95
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-14 16:51:37 -0300

    refactor: enhance LoaderParams type to include optional productId and queryClient

--- src/types/_product.types.ts
+++ src/types/_product.types.ts
@@ -1,12 +1,18 @@
 import type { Product } from "@/types";
+import type { QueryClient } from "@tanstack/react-query";
 
 export type LoaderData = {
   products: Product[];
   category: Product["category"];
+  selectedProduct?: Product | null;
 };
 
 export type LoaderParams = {
   params: {
     category: Product["category"];
+    productId?: Product["id"];
   };
+  context: {
+    queryClient?: QueryClient | undefined;
+  }
 };
