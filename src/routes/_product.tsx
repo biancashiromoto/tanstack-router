460acd30df29a505f21d44dcc0ac95a379e4ed5e
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-14 15:32:00 -0300

    refactor: wrap Outlet in Box component for improved layout and styling

--- src/routes/_product.tsx
+++ src/routes/_product.tsx
@@ -1,9 +1,14 @@
 import { getProductsByCategory } from "@/services/products";
 import type { LoaderData, LoaderParams } from "@/types/_product.types";
+import { Box } from "@mui/material";
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_product")({
-  component: () => <Outlet />,
+  component: () => (
+    <Box sx={{ mx: "auto", py: 2, maxWidth: 1200 }}>
+      <Outlet />
+    </Box>
+  ),
   loader: async ({ params }: LoaderParams): Promise<LoaderData> => {
     const category = params.category;
     const { products } = await getProductsByCategory(category);
