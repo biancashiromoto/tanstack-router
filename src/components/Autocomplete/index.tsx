76378653e87049626059eb19da0bdcc5cc479d3e
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-14 17:02:18 -0300

    refactor: remove unused useLocation import and clean up CustomAutocomplete component

--- src/components/Autocomplete/index.tsx
+++ src/components/Autocomplete/index.tsx
@@ -4,24 +4,16 @@ import { FormControl, Typography } from "@mui/material";
 import Autocomplete from "@mui/material/Autocomplete";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import { useLocation } from "@tanstack/react-router";
 import { useNavigate } from "@tanstack/react-router";
-import { useEffect } from "react";
 
 export default function CustomAutocomplete() {
   const navigate = useNavigate();
-  const location = useLocation();
   const { search, options, handleSearchChange } = useSearchProducts();
   const { isMobile } = useResponsive();
 
   const filteredProducts = options.filter((option) =>
     option.title.toLowerCase().includes(search.toLowerCase())
   );
-
-  useEffect(() => {
-    if (location.pathname === "/" || location.pathname === "/cart")
-      handleSearchChange("");
-  }, [location]);
 
   return (
     <FormControl fullWidth>
