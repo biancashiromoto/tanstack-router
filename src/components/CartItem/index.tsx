ca192a615edae201a885c1886c67425b267c2bcb
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-14 13:26:20 -0300

    fix: refactor CartItem component to remove unnecessary product fetching and simplify click handling

--- src/components/CartItem/index.tsx
+++ src/components/CartItem/index.tsx
@@ -1,37 +1,50 @@
-import { getProductById } from "@/services/products";
-import type { Product } from "@/types"
+import type { Product } from "@/types";
+import { Box, Typography } from "@mui/material";
 import { useNavigate } from "@tanstack/react-router";
-import { useEffect, useState } from "react";
 
 type CartItemProps = {
   item: Product;
-}
-
+};
 
 const CartItem = ({ item }: CartItemProps) => {
-  const [product, setProduct] = useState<Product | null>(null);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const getProduct = async () => {
-      const product = await getProductById(item.id);
-      setProduct(product);
-    }
-    getProduct();
-  }, [item.id]);
+  const handleClick = () =>
+    navigate({
+      to: "/cart/$id",
+      params: { category: item.category, id: item.id.toString() },
+    });
 
-  const handleClick = () => navigate({ to: `/${product?.category}/${product?.id}` });
-  
   return (
-    <div className='cart-item' key={item.id} onClick={handleClick}>
-      <img src={item.thumbnail} alt={item.title} className="cart-thumbnail" />
-      <strong>{item.title}</strong>
-      <span className="cart-item-price">${item.price}</span>
-      <div className="cart-item-quantity">
-        <span className="quantity">{item.quantity}</span>
-      </div>
-    </div>
-  )
-}
+    <Box
+      onClick={handleClick}
+      sx={{
+        cursor: "pointer",
+        display: "flex",
+        alignItems: "center",
+        gap: 2,
+        padding: 1,
+        border: "1px solid #ccc",
+        borderRadius: 1,
+        justifyContent: "space-between",
+      }}
+    >
+      <Box
+        component="img"
+        src={item.thumbnail}
+        alt={item.title}
+        className="cart-thumbnail"
+        width="75px"
+        height="75px"
+        sx={{ objectFit: "cover", borderRadius: 1 }}
+      />
+      <Typography variant="body2" sx={{ flexGrow: 1, maxWidth: "150px" }}>
+        {item.title}
+      </Typography>
+      <Typography>${item.price}</Typography>
+      <Typography sx={{ padding: "0 16px" }}>{item.quantity}</Typography>
+    </Box>
+  );
+};
 
-export default CartItem
+export default CartItem;
