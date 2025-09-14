239a5eed44cc168570fee27e8fe8a1232c59face
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-14 15:33:32 -0300

    refactor: replace navigate with Link for improved routing in CustomCard component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,60 +3,60 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { useNavigate } from "@tanstack/react-router";
+import { Link } from "@tanstack/react-router";
 
 export interface CustomCardProps {
   product: Product;
 }
 
 export default function CustomCard({ product }: CustomCardProps) {
-  const navigate = useNavigate();
   return (
-    <Card
-      sx={{
-        width: "100%",
-        height: "100%",
-        cursor: "pointer",
-      }}
-      onClick={() =>
-        navigate({
-          to: "/$category/$id",
-          params: { category: product.category, id: String(product.id) },
-        })
-      }
+    <Link
+      to="/$category/$id"
+      params={{ category: product.category, id: String(product.id) }}
+      style={{ textDecoration: "none" }}
+      preloadDelay={500}
     >
-      <CardMedia
-        component="img"
-        alt={product.title}
-        image={product.images[0]}
-      />
-      <CardContent
+      <Card
         sx={{
-          display: "grid",
-          gridTemplateColumns: "auto auto",
-          gridTemplateRows: "auto auto",
+          width: "100%",
+          height: "100%",
+          cursor: "pointer",
         }}
       >
-        <Typography
-          gutterBottom
-          variant="body1"
-          component="div"
-          sx={{ gridColumn: "span 2", height: 50 }}
-        >
-          {product.title}
-        </Typography>
-        <Typography
-          variant="body2"
+        <CardMedia
+          component="img"
+          alt={product.title}
+          image={product.images[0]}
+        />
+        <CardContent
           sx={{
-            color: "text.secondary",
-            gridRow: 2,
-            gridColumn: 2,
-            justifySelf: "end",
+            display: "grid",
+            gridTemplateColumns: "auto auto",
+            gridTemplateRows: "auto auto",
           }}
         >
-          U${product.price}
-        </Typography>
-      </CardContent>
-    </Card>
+          <Typography
+            gutterBottom
+            variant="body1"
+            component="div"
+            sx={{ gridColumn: "span 2", height: 50 }}
+          >
+            {product.title}
+          </Typography>
+          <Typography
+            variant="body2"
+            sx={{
+              color: "text.secondary",
+              gridRow: 2,
+              gridColumn: 2,
+              justifySelf: "end",
+            }}
+          >
+            U${product.price}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Link>
   );
 }
