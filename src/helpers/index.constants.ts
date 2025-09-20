cb6bb633514d08d9654349a19962ba7352a38b89
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-20 08:38:48 -0300

    feat: add page constants for routing configuration

---
+++ src/helpers/index.constants.ts
@@ -0,0 +1,11 @@
+import type { PageType } from "./index.types";
+
+export const pages: PageType[] = [
+  { name: "Home", path: "/" },
+  { name: "Profile", path: "/_auth/profile" },
+  { name: "Products", path: "/_product" },
+  { name: "Products in <category>", path: "/_product/:category/" },
+  { name: "Cart", path: "/_cart" },
+  { name: "Login", path: "/_auth/login" },
+  { name: "Register", path: "/_auth/register" },
+];
