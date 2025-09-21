239d504ed3f47a7c3ec528a66238215820bcebe5
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-21 14:55:59 -0300

    feat: add authenticatedRoute property to bottom navigation routes

--- src/components/BottomNavigation/index.constants.ts
+++ src/components/BottomNavigation/index.constants.ts
@@ -7,10 +7,12 @@ export interface BottomNavigationRoute {
   icon: React.ComponentType<{ size?: number | string }>;
   path: string;
   value: number;
+  authenticatedRoute: boolean;
 }
 
 export const bottomNavigationRoutes: BottomNavigationRoute[] = [
-  { label: "Profile", icon: RxAvatar, path: "/profile", value: 0 },
-  { label: "Home", icon: GoHome, path: "/", value: 1 },
-  { label: "Cart", icon: AiOutlineShoppingCart, path: "/cart", value: 2 },
+  { label: "Profile", icon: RxAvatar, path: "/profile", value: 0, authenticatedRoute: true },
+  { label: "Home", icon: GoHome, path: "/", value: 1, authenticatedRoute: false },
+  { label: "Cart", icon: AiOutlineShoppingCart, path: "/cart", value: 2, authenticatedRoute: true },
+  { label: "Login", icon: RxAvatar, path: "/login", value: 3, authenticatedRoute: false }
 ];
