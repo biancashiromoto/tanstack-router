2922b344343ae5a9665408f3f498676846d314d4
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-21 14:55:32 -0300

    feat: implement user-based route filtering in BottomNavigation component

--- src/components/BottomNavigation/index.tsx
+++ src/components/BottomNavigation/index.tsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-
 import { useLocation, useNavigate } from "@tanstack/react-router";
 import {
   bottomNavigationRoutes,
   type BottomNavigationRoute,
 } from "./index.constants";
+import { useAuth } from "@/context/AuthContext";
 
 export default function SimpleBottomNavigation() {
   const navigate = useNavigate();
   const location = useLocation();
+  const { user } = useAuth();
 
   const [value, setValue] = useState<BottomNavigationRoute["value"] | null>(
     null
@@ -22,6 +23,10 @@ export default function SimpleBottomNavigation() {
     );
     setValue(routeIndex !== -1 ? routeIndex : null);
   }, [location.pathname]);
+
+  const memoizedRoutes = user
+    ? bottomNavigationRoutes.filter((route) => route.authenticatedRoute)
+    : bottomNavigationRoutes.filter((route) => !route.authenticatedRoute);
 
   return (
     <BottomNavigation
@@ -38,15 +43,13 @@ export default function SimpleBottomNavigation() {
         backgroundColor: "#fff",
       }}
     >
-      {bottomNavigationRoutes.map((route) => (
+      {memoizedRoutes.map((route) => (
         <BottomNavigationAction
           key={route.value}
           label={route.label}
-          icon={<route.icon size={22} />}
+          icon={<route.icon size={24} />}
           onClick={() => navigate({ to: route.path })}
-          style={{
-            color: value === route.value ? "primary.main" : "text.secondary",
-          }}
+          value={route.value}
         />
       ))}
     </BottomNavigation>
