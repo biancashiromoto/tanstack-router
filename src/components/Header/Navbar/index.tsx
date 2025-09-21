352a374a6e6a3213172fec08a377b60c0ddf8920
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-21 14:55:06 -0300

    refactor: simplify Navbar component by removing responsive checks for links

--- src/components/Header/Navbar/index.tsx
+++ src/components/Header/Navbar/index.tsx
@@ -5,31 +5,26 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 import { GoHome } from "react-icons/go";
 import { VscSignIn, VscSignOut } from "react-icons/vsc";
 import { RxAvatar } from "react-icons/rx";
-import useResponsive from "@/hooks/useResponsive";
 
 const Navbar = () => {
   const { user, logout } = useAuth();
-  const { isDesktop } = useResponsive();
 
   const handleLogout = () => logout();
 
   const NavbarLinks = () => {
-    if (!user) {
+    if (!user)
       return (
         <Link.Root to="/login">
           <Link.Icon icon={<VscSignIn />} />
           <Link.Label>Login</Link.Label>
         </Link.Root>
       );
-    }
     return (
       <>
-        {isDesktop && (
-          <Link.Root to="/cart">
-            <Link.Icon icon={<AiOutlineShoppingCart />} />
-            <Link.Label>Cart</Link.Label>
-          </Link.Root>
-        )}
+        <Link.Root to="/cart">
+          <Link.Icon icon={<AiOutlineShoppingCart />} />
+          <Link.Label>Cart</Link.Label>
+        </Link.Root>
         <Link.Root to="/" handleClick={handleLogout}>
           <Link.Icon icon={<VscSignOut />} />
           <Link.Label>Logout</Link.Label>
@@ -48,18 +43,14 @@ const Navbar = () => {
         justifyContent: user ? "space-between" : "flex-end",
       }}
     >
-      {user && isDesktop && (
-        <Link.Root to="/profile">
-          <Link.Icon icon={<RxAvatar />} />
-          <Link.Label>Profile</Link.Label>
-        </Link.Root>
-      )}
-      {isDesktop && (
-        <Link.Root to="/">
-          <Link.Icon icon={<GoHome />} />
-          <Link.Label>Home</Link.Label>
-        </Link.Root>
-      )}
+      <Link.Root to="/profile">
+        <Link.Icon icon={<RxAvatar />} />
+        <Link.Label>Profile</Link.Label>
+      </Link.Root>
+      <Link.Root to="/">
+        <Link.Icon icon={<GoHome />} />
+        <Link.Label>Home</Link.Label>
+      </Link.Root>
       <NavbarLinks />
     </Box>
   );
