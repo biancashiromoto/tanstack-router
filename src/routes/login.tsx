e566a43ab0f7a7c6be5efbff43c24fd965cbbbd5
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-21 20:49:02 -0300

    refactor: simplify login route by removing RouteComponent and using inline components

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,10 +1,10 @@
+import Loader from "@/components/Loader";
 import LoginForm from "@/components/LoginForm";
+import { getMetaHeader } from "@/helpers";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/login")({
-  component: RouteComponent,
+  component: () => <LoginForm />,
+  pendingComponent: () => <Loader />,
+  head: () => getMetaHeader("Login"),
 });
-
-function RouteComponent() {
-  return <LoginForm />;
-}
