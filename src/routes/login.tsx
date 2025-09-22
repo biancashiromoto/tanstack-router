fe0ab7086f81b3f9f1af2f22db9d3ab63e6d2863
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-21 21:11:23 -0300

    refactor: add redirect logic in login route to prevent access for authenticated users

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,10 +1,13 @@
 import Loader from "@/components/Loader";
 import LoginForm from "@/components/LoginForm";
 import { getMetaHeader } from "@/helpers";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/login")({
   component: () => <LoginForm />,
   pendingComponent: () => <Loader />,
   head: () => getMetaHeader("Login"),
+  beforeLoad: ({ context }) => {
+    if (context?.user?.accessToken) throw redirect({ to: "/profile" });
+  },
 });
