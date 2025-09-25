b16597c0d5a142491975948580bf7fa0a1e142e1
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-25 19:50:22 -0300

    refactor: replace navigate with Link for improved routing in CardRoot component

--- src/components/Card/CardRoot.tsx
+++ src/components/Card/CardRoot.tsx
@@ -1,28 +1,22 @@
-import { useNavigate } from "@tanstack/react-router";
+import { Card as MUICard } from "@mui/material";
+import { Link } from "@tanstack/react-router";
 import { CardContext } from "./context/CardContext";
 import type { ICardRootProps } from "./index.types";
-import { Card as MUICard } from "@mui/material";
 
 const CardRoot = ({
   product,
   shouldShowDiscount = false,
   children,
 }: ICardRootProps) => {
-  const navigate = useNavigate();
-  const handleClick = () =>
-    /**
-     * Navigate com rota e parâmetros tipados
-     */
-    navigate({
-      to: "/$category/$productId",
-      params: { category: product.category, productId: product.id },
-    });
-
   return (
     <CardContext.Provider value={{ product, shouldShowDiscount }}>
-      <MUICard onClick={handleClick} sx={{ cursor: "pointer" }}>
-        {children}
-      </MUICard>
+      <Link
+        to="/$category/$productId"
+        params={{ category: product.category, productId: product.id }}
+        style={{ textDecoration: "none" }}
+      >
+        <MUICard sx={{ cursor: "pointer" }}>{children}</MUICard>
+      </Link>
     </CardContext.Provider>
   );
 };
