7aa55050f670bc0bbe5ad76462bd0d0a9a2f8f8d
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2025-09-25 19:48:03 -0300

    refactor: enhance CardMedia component with lazy loading and improved fallback UI

--- src/components/Card/CardMedia.tsx
+++ src/components/Card/CardMedia.tsx
@@ -1,16 +1,39 @@
 import { useCardContext } from "./context/CardContext";
-import { CardMedia as MUICardMedia } from "@mui/material";
+import { CardMedia as MUICardMedia, Skeleton } from "@mui/material";
+import { Suspense } from "react";
+
+const LazyImage = ({ src, alt }: { src: string; alt: string }) => (
+  <MUICardMedia
+    component="img"
+    image={src}
+    alt={alt}
+    loading="lazy"
+    sx={{
+      width: "100%",
+      height: 200,
+      objectFit: "contain",
+      objectPosition: "center",
+    }}
+  />
+);
 
 const CardMedia = () => {
   const { product } = useCardContext();
+  const imageUrl = product.images?.[0] || product.thumbnail;
 
   return (
-    <MUICardMedia
-      component="img"
-      image={product.images?.[0] || product.thumbnail}
-      alt={product.title}
-      sx={{ width: "100%", height: "auto" }}
-    />
+    <Suspense
+      fallback={
+        <Skeleton
+          variant="rectangular"
+          width="100%"
+          height={200}
+          animation="wave"
+        />
+      }
+    >
+      <LazyImage src={imageUrl || ""} alt={product.title} />
+    </Suspense>
   );
 };
 
